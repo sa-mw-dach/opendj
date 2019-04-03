dd75f2e79128e7d81841720fc792d47329df2c4d
Daniel Fröhlich <daniel.froehlich@redhat.com>
2019-04-04 01:46:14 +0200

    Implementation of currentTrack API

--- services/spotify-provider-boundary/app.js
+++ services/spotify-provider-boundary/app.js
@@ -3,5 +3,6 @@
 const express = require('express');
 const app = express();
 app.use('/play', require('./lib/player.js')());
+app.use('/currentTrack', require('./lib/currentTrack.js')());
 
 module.exports = app;
---
+++ services/spotify-provider-boundary/lib/currentTrack.js
@@ -0,0 +1,81 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var https = require('https');
+
+var fs  = require("fs");
+
+function playerRoute() {
+    var router = new express.Router();
+    router.use(cors());
+    router.use(bodyParser());
+
+    function handleError(err, response) {
+
+        console.log('Error: ' + err);
+
+        var error = {
+            "message": err,
+            "code": 500
+        };
+        response.writeHead(500);
+        response.end(JSON.stringify(error));
+    }
+
+    router.get('/', function (request, response) {
+
+        var token = process.env.token;
+        var device = process.env.device;
+
+        console.log(token);
+        console.log(device);
+
+
+        var options = {
+            hostname: 'api.spotify.com',
+            path: '/v1/me/player/currently-playing',
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Authorization': 'Bearer ' + token
+            }
+        };
+
+        var req = https.request(options, function (res) {
+            console.log('Status: ' + res.statusCode);
+            console.log('Headers: ' + JSON.stringify(res.headers));
+
+            res.setEncoding('utf8');
+
+            var bodyStr = '';
+            res.on('data', function (chunk) {
+              bodyStr += chunk;
+            });
+
+            res.on('end', function () {
+                console.log('BODY: ' + bodyStr);
+                var body = JSON.parse(bodyStr);
+                var myResponse = {
+                  "is_playing": body.is_playing,
+                  "progress_ms":  body.progress_ms,
+                  "duration_ms":  body.item.duration_ms,
+                  "ressourceURI": body.item.uri
+                };
+                console.log('myResponse: ' + myResponse);
+                response.end(JSON.stringify(myResponse));
+            });
+
+        });
+
+        req.on('error', function (e) {
+            handleError(e, response);
+        });
+
+//        req.write();
+        req.end();
+    });
+
+    return router;
+}
+
+module.exports = playerRoute;

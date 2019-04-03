97b5e1677b7913281fce3be7495cfd0fb06c92d2
Colum Bennett <cbennett@redhat.com>
2019-04-03 14:11:26 +0200

    removed node_modules & added gitignore

--- generated-code/node-v3/index.js
+++ generated-code/node-v3/index.js
@@ -36,9 +36,13 @@ oas3Tools.initializeMiddleware(swaggerDoc, function (middleware) {
   app.use(middleware.swaggerUi());
 
   // Start the server
-  http.createServer(app).listen(serverPort, function () {
+  // http.createServer(app).listen(serverPort, function () {
+  //   console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
+  //   console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
+  // });
+
+  app.listen(serverPort, function () {
     console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
     console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
   });
-
 });
--- generated-code/node-v3/node_modules/oas3-tools/README.md
+++
@@ -1,1 +0,0 @@
-This is a forked project from https://github.com/apigee-127/swagger-tools with an attemp to add Open API 3 support. :)

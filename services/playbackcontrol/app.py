8607fc6bc79d6be2cb04ea1b78b1e312dd28ad6d
Wolfram Richter <wrichter@redhat.com>
2019-04-04 10:32:50 +0200

    fix controller polling and break on different track

--- services/playbackcontrol/app.py
+++ services/playbackcontrol/app.py
@@ -9,6 +9,9 @@ boundary_service_url = os.getenv('BOUNDARY_SERVICE_URL','http://spotify-provider
 playlist_service_polling_timeout_msec = int(os.getenv('PLAYLIST_SERVICE_POLLING_TIMEOUT_MSEC','100')) / 1000.0
 backend_service_polling_timeout_msec = int(os.getenv('BACKEND_SERVICE_POLLING_TIMEOUT_MSEC','5000')) / 1000.0
 
+print(playlist_service_polling_timeout_msec)
+print(backend_service_polling_timeout_msec)
+
 try:
     while True:
         # retrieve playlists
@@ -24,31 +27,32 @@ try:
             while True:
                 # retrieve first track from first playlist
                 response = requests.get(playlist_service_url + 'playlists/' + playlists[0]['_id'] + '/firstTrack')
-                print(response)
+                print(response.content)
                 if response.status_code == 404:
                     break;
                 response.raise_for_status()
                 first_track_object = json.loads(response.content)
                 
                 # tell backend to play track
-                response = requests.post(boundary_service_url + 'play', data={"track":first_track_object['_id']})
+                response = requests.post(boundary_service_url + 'play', data={"track":first_track_object['resourceURI']})
                 response.raise_for_status()
 
                 while True:
                     # retrieve player status from backend
                     response = requests.get(boundary_service_url + 'currentTrack')
+                    print(response.content)
                     response.raise_for_status()
                     currentTrack = json.loads(response.content)
 
-                    if (not currentTrack['is_playing']) or (currentTrack['progress_ms'] >= currentTrack['duration_ms']):
+                    if (not currentTrack['is_playing']) or (int(currentTrack['progress_ms']) >= int(currentTrack['duration_ms'])) or (currentTrack['resourceURI'] != first_track_object['resourceURI']):
                         break
                     
                     # sleep some time
                     time.sleep(backend_service_polling_timeout_msec)
 
                 # remove first track from playlist
-                response = requests.delete(playlist_service_url + 'playlists/' + playlists[0]['_id'] + '/tracks/' + first_track_object['resourceURI'])
-                response.raise_for_status()
+                #response = requests.delete(playlist_service_url + 'playlists/' + playlists[0]['_id'] + '/tracks/' + first_track_object['resourceURI'])
+                #response.raise_for_status()
         time.sleep(playlist_service_polling_timeout_msec)
 
 except requests.exceptions.RequestException as e:

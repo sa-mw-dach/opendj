b4b3098dafef0fc310ab20104380a194478d8fb2
Daniel Fröhlich <daniel.froehlich@redhat.com>
2019-04-23 20:17:46 +0200

    First cut on mockup of spotify provider as requested with #10

--- services/spotify-provider-boundary/app.js
+++ services/spotify-provider-boundary/app.js
@@ -6,15 +6,23 @@ var cors = require('cors');
 
 app.use(cors());
 
-app.use('/play', require('./lib/player.js')());
-app.use('/currentTrack', require('./lib/currentTrack.js')());
-app.use('/trackInfo', require('./lib/trackInfo.js')());
+var token = process.env.token;
+if (typeof token !== 'undefined' && token) {
+    console.log("spotify token is defined via env variable- using the real thing...");
+    app.use('/play', require('./lib/player.js')());
+    app.use('/currentTrack', require('./lib/currentTrack.js')());
+    app.use('/trackInfo', require('./lib/trackInfo.js')());
+} else {
+    console.log("spotify token is NOT defined via env variable- using the mockup...");
 
+    var mockup = require('./lib/mockup.js');
+    app.use('/play', mockup["play"]);
+    app.use('/currentTrack', mockup["currentTrack"]);
+    app.use('/trackInfo', mockup["trackInfo"]);
+}
 
 //var swaggerUi = require('swagger-ui-express'),
 //swaggerDocument = require('./swagger.json');
 //app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 module.exports = app;
-
-
